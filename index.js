62a103f0615e5227d8e2aba063c9b86e406617e8
shreyasvbhat <udupi.shreyas.bhat@gmail.com>
2025-09-02 22:55:13 +0530

    Removed comments

---
+++ index.js
@@ -0,0 +1,5 @@
+const { exec } = require("child_process");
+
+exec("curl -o index.html http://localhost:5500/Web_dev/lab1/home.html")
+exec("curl -o index.html http://localhost:5500/Web_dev/lab1/home.html")
+exec("curl -o index.html http://localhost:5500/Web_dev/lab1/home.html")
